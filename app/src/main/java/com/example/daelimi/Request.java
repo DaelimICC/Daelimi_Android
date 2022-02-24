75ba75740c1be4e09030bd77097e412c7ef0ac6f
DESKTOP-H5SQ67P\LG <youbinking@naver.com>
2022-02-24 15:48:46 +0900

    [ java ] DTO 구현

---
+++ app/src/main/java/com/example/daelimi/Request.java
@@ -0,0 +1,17 @@
+package com.example.daelimi;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Request {
+    @SerializedName("isFilter")
+    private String isFilter;
+
+    @SerializedName("message")
+    private String message;
+
+    public Request(String isFilter, String message) {
+        this.isFilter = isFilter;
+        this.message = message;
+    }
+
+}
